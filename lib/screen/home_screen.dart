c3865c85e13e79f28fdf51ba38dca1455d87f3bd
donghun <hoonyhoeny@gmail.com>
2022-08-01 22:45:21 +0900

    -Drawer작성 -캐스케이딩 활용하여 리스트작성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,27 @@
+
+import 'package:dusty_dust/component/main_app_bar.dart';
+import 'package:dusty_dust/component/main_drawer.dart';
+import 'package:dusty_dust/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  final ts = const TextStyle(
+    color: Colors.white,
+    fontSize: 30.0
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      backgroundColor: primaryColor,
+      drawer: MainDrawer(
+
+      ),
+      body: CustomScrollView(
+        slivers: [
+          MainAppBar(
+          ),
+        ],
       ),
     );
   }
